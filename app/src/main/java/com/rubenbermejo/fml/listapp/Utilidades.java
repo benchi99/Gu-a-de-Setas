e486289b121f66f1567c159f3bb771cff832c45b
Rubén (benchi99) Bermejo <mejoruben@gmail.com>
2019-02-26 13:24:44 +0100

    Permisos de Internet, Eliminado contenido del ContentProvider, comenzado consumir REST
    Para poder comenzar a consumir el Servicio REST

--- app/src/main/java/com/rubenbermejo/fml/listapp/Utilidades.java
+++ app/src/main/java/com/rubenbermejo/fml/listapp/Utilidades.java
@@ -1,14 +1,8 @@
 package com.rubenbermejo.fml.listapp;
 
-import android.content.ContentResolver;
-import android.content.ContentValues;
 import android.content.Context;
-import android.content.res.Resources;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.widget.Toast;
 
 import java.io.ByteArrayInputStream;
@@ -24,49 +18,39 @@ public class Utilidades {
         this.context = context;
     }
 
-
-    //Tablas
-    final public static String NOMBRE_TABLA = "SETAS";
-
     //Atributos
     final public static String NOMBRE_COLUMNA = "nombre";
     final public static String DESCRIPCION_COLUMNA = "descripcion";
-    final public static String NOMBRECOMUN_COLUMNA = "nombreComun";
-    final public static String URLLINEA_COLUMNA = "URLlinea";
+    final public static String NOMBRECOMUN_COLUMNA = "nombre_comun";
+    final public static String URLLINEA_COLUMNA = "url";
     final public static String COMESTIBLE_COLUMNA = "comestible";
     final public static String ID_COLUMNA = "id";
     final public static String FAV_COLUMNA = "favorito";
     final public static String IMG_COLUMNA = "imagen";
-    final private static String AUTHORITY = "com.rubenbermejo.fml.contentprovidersetas";
-    final public static String uRI = "content://" + AUTHORITY + "/" + NOMBRE_TABLA;
-    final public static Uri CONTENT_URI = Uri.parse(uRI);
+    final public static String DIRECCION_REST_LOCAL = "http://localhost/ServicioREST";
+    final public static String DIRECCION_REST_DATAUS = "http://datauschwitz-se/practicasRuben/ServicioREST";
+    final public static String DIRECCION_REST_MARISMA = "http://dam2.ieslamarisma.net/2019/rubenbermejo";
+    final public static String POST_GET_ALL = "/setas";
+    final public static String GET_FAV = "/setas/favoritos";
+    final public static String GET_PUT_DELETE_ID = "/setas/";
+
+
     final public static String NORMAL = "normal";
     final public static String FAVORITOS = "favorito";
 
 
     public static ArrayList<ObjetoSetas> obtenerListaMasReciente(Context context, String param) {
 
-        String[] params = { "1" };
-        String[] cols = { "*" };
-
-        ContentResolver cr = context.getContentResolver();
-        Cursor c;
-
         if (param.equals(NORMAL)){
-            c = cr.query(CONTENT_URI, cols, null, null, null);
+
         } else {
-            c = cr.query(CONTENT_URI, cols, Utilidades.FAV_COLUMNA + " = ?", params, null);
+
         }
 
         ArrayList<ObjetoSetas> listActual = new ArrayList<>();
         ObjetoSetas seta;
 
-        while (c.moveToNext()) {
-            seta = new ObjetoSetas(c.getString(1), c.getString(2), c.getString(3), c.getString(4), intToBool(Integer.parseInt(c.getString(5))), c.getBlob(7));
-            seta.setId(c.getInt(0));
-            listActual.add(seta);
-        }
-        return listActual;
+    return listActual;
    }
 
     public static byte[] convertirImagenABytes(Bitmap bmp) {
@@ -94,11 +78,7 @@ public class Utilidades {
     }
 
     public static void delElement(Context context, int id){
-        String[] param = { String.valueOf(id) };
 
-        ContentResolver cr = context.getContentResolver();
-
-        cr.delete(CONTENT_URI, Utilidades.ID_COLUMNA + " = ?", param);
     }
 
 }
